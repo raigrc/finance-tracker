5a7ab4ca8914d6b2a1c2abfb4b64450e6df51c3d
raigrc <ravenjstn.grc@gmail.com>
2024-08-01 18:22:36 +0800

    add more security to login

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,5 +8,14 @@ export const LoginSchema = z.object({
 export const RegisterSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   email: z.string().email({ message: "Email is required" }),
-  password: z.string().min(6, { message: "Password is required" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(8, { message: "Password must be less than 32 characters" })
+    .regex(/[A-Z]/, { message: "Password must contain at least 1 uppercase" })
+    .regex(/[a-z]/, { message: "Password must contain at least 1 lowercase" })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[\W_]/, {
+      message: "Password must contain at least one special character",
+    }),
 });
