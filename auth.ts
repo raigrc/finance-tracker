48f30336a28b2f3c26cd94f3f40b3372ee6ceb61
raigrc <ravenjstn.grc@gmail.com>
2024-08-03 15:52:27 +0800

    fix token expiration

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/lib/prisma";
 import authConfig from "@/auth.config";
 import { UserRole } from "@prisma/client";
 import { getSessionByUserId, getUserById } from "./data/user";
-import { uuid } from "uuidv4";
+import { v4 as uuidv4 } from "uuid";
 
 declare module "next-auth" {
   interface Session {
@@ -21,8 +21,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
-    updateAge: 24 * 60 * 60,
   },
   ...authConfig,
   callbacks: {
@@ -31,24 +29,38 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         session.user.id = token.sub;
       }
 
-      // if (session.user && token.role) {
-      //   session.user.role = token.role as UserRole;
-      // }
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
 
-      // console.log({ session });
+      session.expires = token.expires as Date & string;
+
+      console.log({ session });
 
       return session;
     },
     async jwt({ token, user }) {
+      const now = new Date();
+      const expires = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        23,
+        59,
+        59,
+      );
       if (user) {
         token.sub = user.id;
         token.role = user.role;
 
-        const sessionToken = uuid();
-        const expires = new Date(Date.now() + 30 * 24 * 60 * 60);
+        console.log({ expires });
+
+        token.iat = Math.floor(Date.now() / 1000);
+        token.exp = Math.floor(expires.getTime() / 1000);
 
         if (!token.sub) return token;
 
+        const sessionToken = uuidv4();
         const sessionTokenData = await getSessionByUserId(token.sub);
 
         if (sessionTokenData) {
@@ -66,10 +78,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
             expires,
           },
         });
-        console.log({ user });
 
-        console.log({ token });
+        token.expires = expires;
+
+        if (token.exp && Date.now() / 1000 > token.exp) {
+          // await prisma.session.deleteMany({});
+          return {};
+        }
+
+        console.log({ userData: user });
       }
+
+      console.log({ token, expires });
 
       return token;
     },
